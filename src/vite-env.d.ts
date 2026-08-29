a2cf942ba0ed11fe7aaf6ef549150a89cdad07a0
adamaf12 <maamarfeidat@gmail.com>
2026-08-29 01:05:27 +0100

    chore: define v2 client environment typing

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,16 +1,9 @@
 /// <reference types="vite/client" />
 
-declare module '*.jpg' {
-  const src: string;
-  export default src;
+interface ImportMetaEnv {
+  readonly VITE_ADAM_API_URL?: string;
 }
 
-declare module '*.png' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.svg' {
-  const src: string;
-  export default src;
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
